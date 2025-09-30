c4cea132236247bf42f9753fe4e696255e17b745
itskk <kalpatarusethi34@gmail.com>
2025-09-30 22:53:38 +0530

    improvements

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["https://tube-tweet-kohl.vercel.app","http://localhost:5173"],
     credentials: true 
 }))
 
