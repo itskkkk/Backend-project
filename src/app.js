c0dae42c06af28b8e4a6dfa8633ab142ed0ce567
itskk <kalpatarusethi34@gmail.com>
2025-10-06 16:00:51 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(morgan("dev"))
 
 //routes import
 
